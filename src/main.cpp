c2d96fd07ff8f57a813d0419527f242b39fcd018
Nicolas Boisvert <nicklay@me.com>
2021-10-25 20:03:06 -0400

    knobs - Working on knob mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Rotary.h>
+#include <FakeController.h>
 #include <Constants.h>
 #include <MidiNotes.h>
 #include <IndividualLeds.h>
@@ -15,22 +16,16 @@
 
 int scale[] = D_MINOR_SCALE_MIDI;
 // int leds[] = {LED_0, LED_1, LED_2, LED_3, LED_4, LED_5, LED_6, LED_7};
-int bottomLeds[] = {MUX_1_1, MUX_1_2, MUX_1_3};
-int topLeds[] = {MUX_2_1, MUX_2_2, MUX_2_3};
+int bottomLeds[] = {2, 3, 4};
 
 Sequencer::Application *application;
 
 void setup()
 {
-  Sequencer::Controller *controller = new Sequencer::Rotary(PLAY_PIN, CLK_PIN, DT_PIN, MODE_SELECT_PIN, TEMPO_POT_PIN, CLOCK_GATE_POT_PIN, RANDOM_PIN);
-  //Sequencer::IndividualLeds *ledStrip = new Sequencer::IndividualLeds(leds);
-  Sequencer::Multiplexed8Bit *ledStrip = new Sequencer::Multiplexed8Bit(topLeds, bottomLeds);
-  Sequencer::MidiOutput *midiOut = new Sequencer::MidiOutput(SERIAL_TX);
+  Sequencer::FakeController *controller = new Sequencer::FakeController(A2, A3);
   Sequencer::ControlVoltage *controlVoltage = new Sequencer::ControlVoltage(DIGITAL_ANALOG_CONVERTER_1, TRIG_PIN, GATE_PIN);
-  Sequencer::MultiNoiseMaker *noiseMaker = new Sequencer::MultiNoiseMaker(midiOut, controlVoltage);
-  //Sequencer::SerialNoiseMaker *serial = new Sequencer::SerialNoiseMaker();
-  //Sequencer::MultiNoiseMaker *noiseMaker = new Sequencer::MultiNoiseMaker(serial, controlVoltage);
-  application = new Sequencer::Application(scale, ledStrip, controller, noiseMaker);
+  Sequencer::LedStrip *ledStrip = new Sequencer::Multiplexed8Bit(bottomLeds, bottomLeds);
+  application = new Sequencer::Application(scale, ledStrip, controller, controlVoltage);
 }
 
 void loop()
